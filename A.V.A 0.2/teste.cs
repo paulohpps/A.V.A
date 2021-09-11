b1ef045fc41c90831984dc9ad81f8aae5b58ac2a
Paulo Henrique <paulohpps13@gmail.com>
2021-09-11 18:22:05 -0300

    A.V.A Vesion 0.2.1

---
+++ A.V.A 0.2/teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.V.A_0._2
+{
+    class teste
+    {
+    }
+}
